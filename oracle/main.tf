1e413c44723047d10c62e844a156c4d0913f0d89
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-08-19 11:14:56 -0700

    working ingress

--- oracle/main.tf
+++ oracle/main.tf
@@ -11,16 +11,7 @@ resource "oci_core_internet_gateway" "internet_gateway" {
   display_name = "Homelab Gateway"
   enabled = true
 }
-
-resource "oci_core_subnet" "homelab_subnet" {
-  display_name              = "Homelab Subnet"
-  cidr_block                = "10.0.69.0/24"
-  compartment_id            = var.tenancy_ocid
-  vcn_id                    = oci_core_vcn.homelab_vcn.id
-  prohibit_internet_ingress = false
-}
-
-resource "oci_core_route_table" "test_route_table" {
+resource "oci_core_route_table" "route_table" {
   compartment_id = var.tenancy_ocid
   vcn_id = oci_core_vcn.homelab_vcn.id
   display_name = "Homelab Gateway Route Table"
@@ -30,6 +21,15 @@ resource "oci_core_route_table" "test_route_table" {
       destination = "0.0.0.0/0"
       destination_type = "CIDR_BLOCK"
   }
+}
+
+resource "oci_core_subnet" "homelab_subnet" {
+  display_name              = "Homelab Subnet"
+  cidr_block                = "10.0.69.0/24"
+  compartment_id            = var.tenancy_ocid
+  vcn_id                    = oci_core_vcn.homelab_vcn.id
+  prohibit_internet_ingress = false
+  route_table_id            = oci_core_route_table.route_table.id
 }
 
 data "oci_identity_availability_domains" "availability_domains" {
