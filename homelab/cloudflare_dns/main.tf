2d26964f9ee21870cfcb275cd9b537ad49a2217c
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-08-21 14:46:27 -0700

    cloudflare settings

--- homelab/cloudflare_dns/main.tf
+++ homelab/cloudflare_dns/main.tf
@@ -1,3 +1,18 @@
 resource "cloudflare_zone" "pleasenoddos_zone" {
   zone = "pleasenoddos.com"
 }
+
+resource "cloudflare_zone_settings_override" "pleasenoddos_settings" {
+  zone_id = cloudflare_zone.pleasenoddos_zone.id
+  settings {
+    always_use_https = "on"
+    websockets = "on"
+    ssl = "strict"
+    min_tls_version = "1.2"
+    minify {
+      css = "on"
+      js = "off"
+      html = "off"
+    }
+  }
+}
