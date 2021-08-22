b224aae0141f50af5badefabb282c83d810554e5
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-08-21 19:06:39 -0700

    separate out statefile url and add file templates

--- homelab/versions.tf
+++ homelab/versions.tf
@@ -17,11 +17,5 @@ terraform {
       version = "~> 4.40"
     }
   }
-  backend "http" {
-    # Update address and ignore changes with `git update-index --skip-worktree versions.tf`
-    # Undo ignore to commit new changes with `git update-index --no-skip-worktree versions.tf`
-    address       = "https://objectstorage.ca-toronto-1.oraclecloud.com/p/xehE9K_iQ5DhoK_O7zMjOWzRQt3w2IYSUzVt49p7WGSNjX1QmWVOJ1cR_zCJQMda/n/yzitcaqqr4r5/b/terraform-state/o/terraform.tfstate"
-    update_method = "PUT"
-  }
   required_version = "~> 1.0.4"
 }
