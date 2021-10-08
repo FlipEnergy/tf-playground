e741ee24fac817fe9c306f191a8a50ed16afb5e5
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-10-08 14:57:16 -0700

    remove estimate budget

--- homelab/oracle/budget.tf
+++ homelab/oracle/budget.tf
@@ -15,12 +15,3 @@ resource "oci_budget_alert_rule" "homelab_budget_alert" {
   type           = "ACTUAL"
   recipients     = var.my_email
 }
-
-resource "oci_budget_alert_rule" "homelab_forecast_alert" {
-  display_name   = "homelab-forecast-alert"
-  budget_id      = oci_budget_budget.homelab_budget.id
-  threshold      = 0.10
-  threshold_type = "PERCENTAGE"
-  type           = "FORECAST"
-  recipients     = var.my_email
-}
