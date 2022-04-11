99846cbe4f4ca0663588cbb65fdbcbaec7b0cf79
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2022-04-10 22:23:00 -0700

    fix tls, update IP

--- homelab/kubernetes/main.tf
+++ homelab/kubernetes/main.tf
@@ -5,8 +5,8 @@ resource "kubernetes_secret" "default_ingress_nginx_tls" {
   }
 
   data = {
-    "tls.crt" = base64encode(var.cert)
-    "tls.key" = base64encode(var.key)
+    "tls.crt" = var.cert
+    "tls.key" = var.key
   }
 
   type = "kubernetes.io/tls"
